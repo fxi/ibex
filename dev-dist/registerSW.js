c648a609e179bcd3e90ba26448ce67cf5c48779b
fxi <f@fxi.io>
2025-08-06 22:52:55 +0200

    feat: pwa + icons

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/ibex/dev-sw.js?dev-sw', { scope: '/ibex/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,67 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
-
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: '/ibex/',
-    plugins: [react(), tailwindcss()],
+    base: "/ibex/",
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+        manifest: {
+          name: "Ibex",
+          short_name: "Ibex",
+          description: "Cyclers api based client",
+          theme_color: "#1c1917",
+          start_url: "/ibex/",
+          scope: "/ibex/",
+          display: "standalone",
+          background_color: "#1c1917",
+          icons: [
+            {
+              src: "pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any",
+            },
+            {
+              src: "maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+        devOptions: {
+          enabled: true,
+        },
+      }),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
     },
     define: {
-      'import.meta.env.VITE_MAPTILER_API_KEY': JSON.stringify(env.VITE_MAPTILER_API_KEY)
-    }
-  }
-})
+      "import.meta.env.VITE_MAPTILER_API_KEY": JSON.stringify(
+        env.VITE_MAPTILER_API_KEY
+      ),
+    },
+  };
+});
